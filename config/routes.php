d1c5359f5459749d2a028e59098acd6ecf6df668
Igor <igorwebber21@gmail.com>
2023-05-22 22:55:44 +0300

    Test commit

--- config/routes.php
+++ config/routes.php
@@ -5,7 +5,7 @@
     // special routes for site
     Router::add('^service/(?P<alias>[a-z0-9-]+)/?$', ['controller' => 'Blog', 'action' => 'view']);
     # Router::add('^category/(?P<alias>[a-z0-9-]+)/?$', ['controller' => 'Category', 'action' => 'view']);
-    # Router::add('^price-list/?$', ['controller' => 'Prices', 'action' => 'view']);
+    # Router::add('^price-list/?$', ['controller' => 'Prices', 'action' => 'view']);#
 
     // default routes for admin
     Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
